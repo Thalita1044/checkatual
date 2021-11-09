2e0d95cf4acca4dd3414039f230bb17d538133ba
Thalita Machado <61170238+Thalita1044@users.noreply.github.com>
2021-11-09 20:22:54 -0300

    Add files via upload

---
+++ checkpoit/check.js
@@ -0,0 +1,73 @@
+let microondas = (food, time ) => {
+  if (food === "pipoca" && time >= 10 && time < 20) {
+    console.log("comida pta, bom apetite");
+  }else if (food === "pipoca" && time >= 20 && time < 30 ){
+    console.log("xiii!Comida queimada")
+  } else if (food == "pipoca" && time < 10) {
+    console.log("precisa de mais tempo");
+  } else if (food == "pipoca" && time >= 30) {
+    console.log("Kabummm!");
+  } else {
+    console.log("prato inexistente");
+  }
+  };
+  let feijao = (food, time) =>{
+    if(food === "feijao" && time >= 12 && time < 24) {
+        console.log ("comida pronta, bom apetite");
+
+  }else if (food === "feijao" && time >= 24 && time< 36) {
+    console.log("xiii!Comida passou do ponto e queimou");
+
+  }  else if (food == "feijão" && time < 12) {
+      console.log("precisa de mais tempo");
+    } else if (food == "feijão" && time >= 36) {
+      console.log ("Kabummm!");
+    } else {
+      consonle.log("prato inexistente")
+    }
+    
+  };
+
+  let carne = (food, time) => {
+    if (food == "carne" && time >= 15 && time < 30) {
+      console.log("Prato pronto, bom apetite!!!");
+    } else if (food == "carne" && time >= 30 && time < 45) {
+      return "xiii!A comida passou do ponto e queimou!";
+    } else if (food == "carne" && time < 15) {
+      console.log("precisa de mais tempo")
+    } else if (food == "carne" && time >= 45) {
+      console.log ("Kabummm!")
+    } else {
+      console.log("prato inexistente");
+    }
+    };
+   
+      let brigadeiro = (food, time) => {
+        if (food == "brigadeiro" && time >= 8 && time < 16) {
+          console.log("Prato pronto, bom apetite!!!");
+        } else if (food == "brigadeiro" && time>= 16 && time < 24) {
+          console.log("xiii!A comida passou do ponto e  queimou!");
+        } else if (food == "brigadeiro" && time < 8) {
+          console.log("Tempo insuficiente");
+        } else if (food == "brigadeiro" && time >= 24) {
+          console.log("Kabummm!");
+        } else {
+          console.log("Prato Inexistente");
+        }
+        };
+
+       let macarrao = (food, time) => {
+        if (food === "macarrão" && time >= 8 && time < 16) {
+          console.log ("Prato pronto, bom apetite!!!");
+        } else if (food == "macarrão" && time >= 16 && time < 24) {
+          console.log("xiii!! A comida passou do ponto e  queimou!");
+        } else if (food == "macarrão" && time < 8) {
+          console.log ("Tempo insuficiente");
+        } else if (food == "macarrão" && time >= 24) {
+          console.log ("Kabummm!");
+        } else {
+          console.log("Prato Inexistente");
+        }
+        };
+        
+    
